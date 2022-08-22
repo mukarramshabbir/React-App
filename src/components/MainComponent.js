05881efbe7001462618638a3a871eb316254b688
Mukarram Ali <mukarramali623@gmail.com>
2022-08-22 10:25:31 -0700

    SPA part 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,7 @@ import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import About from './AboutComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
@@ -36,13 +37,25 @@ class Main extends Component {
         />  
       );
     }
+
+    const DishWithId=({match}) =>{
+      return(
+        <DishDetail dish={this.state.dishes.filter((dish)=>dish.id===parseInt(match.params.dishId,10))[0]}
+          comments={this.state.comments.filter((comment) =>comment.dishId===parseInt(match.params.dishId,10))}
+        />
+      );
+    }
+
     return (
       <div >
         <Header />
         <Switch>
           <Route path="/home" component={HomePage} />
+          <Route path="/aboutus" component={() => <About leaders={this.state.leaders} />} />
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+          <Route path="/menu/:dishId" component={DishWithId} />
           <Route exact path="/contactus" component={Contact} />
+          
           <Redirect to="/home" />
         </Switch>
         <Footer />
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { Card,CardImg,CardImgOverlay,CardText,CardBody,CardTitle,Media } from 'reactstrap';
+import { Card,CardImg,CardImgOverlay,CardText,CardBody,CardTitle,Breadcrumb,BreadcrumbItem } from 'reactstrap';
+import {Link } from 'react-router-dom'; 
 import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
 
     function RenderMenuItem({dish,onClick}){
         return(
-            <Card onClick={()=> onClick(dish.id)}>
-                <CardImg width="100%" object src={dish.image} alt={dish.name}/>
+            <Card >
+                <Link to={`/menu/${dish.id}`}>
+                 <CardImg width="100%" object src={dish.image} alt={dish.name}/>
                 <CardImgOverlay>
                     <CardTitle>{dish.name}</CardTitle>
                 </CardImgOverlay>          
+                </Link>
             </Card>
         );
     }
@@ -18,13 +21,27 @@ import { DISHES } from '../shared/dishes';
         const menu= props.dishes.map((dish)=>{
             return(
                 <div key={dish.id} className="col-12 col-md-5 m-1">      
-                    <RenderMenuItem dish={dish} onClick={props.onClick} />   
+                    <RenderMenuItem dish={dish}  />   
                 </div>
             );
         });
         console.log('Menu component render is invoked')
         return(
             <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem>
+                            <Link to='/home'>Home</Link>
+                        </BreadcrumbItem>
+                        <BreadcrumbItem active>
+                            Menu
+                        </BreadcrumbItem>
+                    </Breadcrumb>
+                        <div className="col-12">
+                            <h3>Menu</h3>
+                            <hr />
+                        </div>
+                </div>
                 <div className="row">
                         {menu}
                 </div>
